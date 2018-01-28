bf00c2de6769155a50a88a1b1a0d729db6fa745e
KevinOCM99 <dbaami@hotmail.com>
2018-01-28 11:47:57 +0800

    gg script

--- ggini.sh
+++ ggini.sh
@@ -2,8 +2,6 @@ export GG_HOME=/u01/app/ogg
 export LD_LIBRARY_PATH=$ORACLE_HOME/lib:/lib:/usr/lib:$GG_HOME
 export CLASSPATH=$ORACLE_HOME/jdk/jre:$ORACLE_HOME/jlib:$ORACLE_HOME/rdbms/jlib
 export PATH=$ORACLE_HOME/bin:$HOME/bin:$GG_HOME:$PATH
-chmod +x gg*
-cp ./gg* $GG_HOME
+chmod +x gg*.kk
+cp ./gg*.kk $GG_HOME/gg*
 cd $GG_HOME
-mv gg*.kk gg*
-./ggsci
