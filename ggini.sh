87530e331bd5cf23c5cd9fcd00f6731612b0c4d8
KevinOCM99 <dbaami@hotmail.com>
2018-01-28 12:06:57 +0800

    gg

--- ggini.sh
+++ ggini.sh
@@ -3,5 +3,8 @@ export LD_LIBRARY_PATH=$ORACLE_HOME/lib:/lib:/usr/lib:$GG_HOME
 export CLASSPATH=$ORACLE_HOME/jdk/jre:$ORACLE_HOME/jlib:$ORACLE_HOME/rdbms/jlib
 export PATH=$ORACLE_HOME/bin:$HOME/bin:$GG_HOME:$PATH
 chmod +x gg*.kk
-cp ./gg*.kk $GG_HOME/gg*
+cp ./gg*.kk $GG_HOME
 cd $GG_HOME
+find -type f -name 'gg*.kk' | while read f; do mv "$f" "${f%.kk}"; done
+echo "cd $GG_HOME"
+
