9f666f8f13ec459a6a54881cd1dca72e37a98bad
Kevin Zhang <dbaami@hotmail.com>
2018-01-29 06:17:27 -0500

    uptime adding timze zone

--- uptime.sql
+++ uptime.sql
@@ -2,8 +2,12 @@ col Instance_Name for a20 heading Instance_Name
 col Started_At for a20
 col "Database_Uptime" for a40
 col current_scn for 999999999999999
+col SESSIONTIMEZONE for a10
+col DBTIMEZONE for a10
+col SESSIONTIMEZONE for a10 heading MYTIMEZONE
 select instance_name,
 to_char(startup_time,'DD-MON-YYYY HH24:MI:SS') as "Started_At",
 timestamp_to_scn(sysdate) current_scn,
 floor(sysdate - startup_time) || 'days(s) '|| trunc( 24*((sysdate-startup_time) - trunc(sysdate-startup_time))) || 'hour(s) '|| mod(trunc(1440*((sysdate-startup_time) - trunc(sysdate-startup_time))), 60) ||'minute(s) '|| mod(trunc(86400*((sysdate-startup_time) - trunc(sysdate-startup_time))), 60) ||'seconds'as "Database_Uptime"
+,CURRENT_DATE, DBTIMEZONE, SESSIONTIMEZONE
 FROM v$instance;
