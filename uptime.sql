c877f0f6a90e7404145a85181839d50ce28ea6ec
KevinOCM99 <dbaami@hotmail.com>
2018-01-29 10:14:20 -0500

    uptime change column order

--- uptime.sql
+++ uptime.sql
@@ -7,7 +7,7 @@ col DBTIMEZONE for a10
 col SESSIONTIMEZONE for a10 heading MYTIMEZONE
 select instance_name,
 to_char(startup_time,'DD-MON-YYYY HH24:MI:SS') as "Started_At",
-timestamp_to_scn(sysdate) current_scn,
 floor(sysdate - startup_time) || 'days(s) '|| trunc( 24*((sysdate-startup_time) - trunc(sysdate-startup_time))) || 'hour(s) '|| mod(trunc(1440*((sysdate-startup_time) - trunc(sysdate-startup_time))), 60) ||'minute(s) '|| mod(trunc(86400*((sysdate-startup_time) - trunc(sysdate-startup_time))), 60) ||'seconds'as "Database_Uptime"
+timestamp_to_scn(sysdate) current_scn,
 ,CURRENT_DATE, DBTIMEZONE, SESSIONTIMEZONE
 FROM v$instance;
