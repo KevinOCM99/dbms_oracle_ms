139f8e696fecf42cce097ebab271a77550448305
KevinOCM99 <dbaami@hotmail.com>
2018-01-31 06:00:37 -0500

    uptime

--- uptime.sql
+++ uptime.sql
@@ -8,6 +8,6 @@ col SESSIONTIMEZONE for a10 heading MYTIMEZONE
 select instance_name,
 to_char(startup_time,'DD-MON-YYYY HH24:MI:SS') as "Started_At",
 floor(sysdate - startup_time) || 'days(s) '|| trunc( 24*((sysdate-startup_time) - trunc(sysdate-startup_time))) || 'hour(s) '|| mod(trunc(1440*((sysdate-startup_time) - trunc(sysdate-startup_time))), 60) ||'minute(s) '|| mod(trunc(86400*((sysdate-startup_time) - trunc(sysdate-startup_time))), 60) ||'seconds'as "Database_Uptime"
-timestamp_to_scn(sysdate) current_scn,
-,CURRENT_DATE, DBTIMEZONE, SESSIONTIMEZONE
+,timestamp_to_scn(sysdate) current_scn,
+CURRENT_DATE, DBTIMEZONE, SESSIONTIMEZONE
 FROM v$instance;
