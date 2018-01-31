de45b219db92ab416283f9ea9c7e38f9d671551b
KevinOCM99 <dbaami@hotmail.com>
2018-01-31 10:18:33 +0800

    archive log

---
+++ arch.sql
@@ -0,0 +1,6 @@
+col name for a50 heading archive_name
+col IS_RECOVERY_DEST_FILE heading IN_FRA for a6
+col STATUS for a6
+select FIRST_CHANGE#,NEXT_CHANGE#,sequence#,thread#,dest_id,recid, STATUS,IS_RECOVERY_DEST_FILE,COMPRESSED,name
+from v$archived_log
+order by FIRST_CHANGE#;
