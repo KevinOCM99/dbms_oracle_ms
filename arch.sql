783d5427739770020bd273d0499c00968dc6bf29
KevinOCM99 <dbaami@hotmail.com>
2018-02-01 01:50:31 -0500

    02/01/18 01:50:31

--- arch.sql
+++ arch.sql
@@ -1,6 +1,9 @@
 col name for a50 heading archive_name
 col IS_RECOVERY_DEST_FILE heading IN_FRA for a6
 col STATUS for a6
+select * 
+from (
 select FIRST_CHANGE#,NEXT_CHANGE#,sequence#,thread#,dest_id,recid, STATUS,IS_RECOVERY_DEST_FILE,COMPRESSED,name
 from v$archived_log
-order by FIRST_CHANGE#;
+order by FIRST_CHANGE# desc) a
+where rownum <= 5;
