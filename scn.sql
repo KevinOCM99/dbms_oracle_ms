f8b5d0b2ae5a66b99fa91bf2c501f1f83bc33ac7
KevinOCM99 <dbaami@hotmail.com>
2018-02-02 02:12:37 -0500

    02/02/18 02:12:37

--- scn.sql
+++ scn.sql
@@ -1,6 +1,6 @@
 select current_scn from v$database;
 
-col HEADER_UPDATED_AT for a20
+col HEADER_UPDATED_AT for a19 heading HEADER_UPDATED_TIME
 select df.name,df.checkpoint_change# SCN_IN_CTRLFILE,dfh.checkpoint_change# SCN_OF_HEADER,to_char(SCN_TO_TIMESTAMP(dfh.checkpoint_change#),'YYYY-MM-DD HH24:MI:SS') HEADER_UPDATED_AT,df.LAST_CHANGE# LAST_SCN
 from v$datafile df, v$datafile_header dfh
 where df.FILE# = dfh.FILE#
