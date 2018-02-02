9d04f14c3fa26e6bf1e669e69469a96c1716267e
KevinOCM99 <dbaami@hotmail.com>
2018-02-02 04:11:04 -0500

    02/02/18 04:11:04

---
+++ loop1.sql
@@ -0,0 +1,27 @@
+set feed off
+set head off
+set echo off
+set term off
+set linesize 120
+set verify off
+spool myloop_tmp.sql
+set feedback off
+set feed off
+set serveroutput on 
+select cmd from (
+select '@' || '&1'  as cmd from dual
+union all
+select 'host echo' as cmd from dual
+union all
+select 'clear scr' as cmd from dual
+) , (select rownum from dual connect by level <=&3) ;
+spool off
+set term on
+set serveroutput on
+set head on
+--clear scr
+@login
+clear scr
+@myloop_tmp.sql
+host rm myloop_tmp.sql
+
