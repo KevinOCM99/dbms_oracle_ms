744b38a1d875d3dba2e12878ae977ce58810d952
KevinOCM99 <dbaami@hotmail.com>
2018-02-11 20:25:58 -0500

    02/11/18 20:25:58

--- gglogd.sql
+++ gglogd.sql
@@ -5,5 +5,5 @@ supplemental_log_data_fk  "Foregin Key",
 supplemental_log_data_all "All"
 FROM   v$database;
 
-select name,log_mode from v$database;
+select name,log_mode,force_logging from v$database;
 
