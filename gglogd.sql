06fa970b00886dd46b3be77f725337e5d33f6125
KevinOCM99 <dbaami@hotmail.com>
2018-02-11 20:31:28 -0500

    02/11/18 20:31:28

--- gglogd.sql
+++ gglogd.sql
@@ -1,3 +1,5 @@
+select 'Show log level for database level, next...gglogs' name from dual;
+
 select supplemental_log_data_min "Minimum",
 supplemental_log_data_pk  "Primary key",
 supplemental_log_data_ui  "Unique Key",
--- gglogt.sql
+++ gglogt.sql
@@ -1,3 +1,5 @@
+select 'Show log level for table level' name from dual;
+
 select OWNER,TABLE_NAME,LOG_GROUP_NAME,LOG_GROUP_TYPE 
 from dba_log_groups
 order by 1,2;
