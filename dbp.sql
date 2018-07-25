5090e12892fb1cbbd9b27e5f16feb73c0d67ac01
KevinOCM99 <dbaami@hotmail.com>
2018-07-25 06:39:48 -0400

    db property

--- dbp.sql
+++ dbp.sql
@@ -3,5 +3,5 @@ col property_name for a45;
 col PROPERTY_VALUE for a85;
 SELECT property_name,property_value
 FROM database_properties                
-WHERE upper(property_name) like '%&prop_name%'
+WHERE upper(property_name) like upper('%&prop_name%')
 order by 1;
