6d005670972a9482fb0086a68080ddfb7593931e
KevinOCM99 <dbaami@hotmail.com>
2018-07-25 06:38:43 -0400

    db property

--- dbp.sql
+++ dbp.sql
@@ -1,8 +1,7 @@
 set lines 180
 col property_name for a45;
-col PROPERTY_VALUE for a45;
-col DESCRIPTION for a50;
-SELECT *
+col PROPERTY_VALUE for a85;
+SELECT property_name,property_value
 FROM database_properties                
 WHERE upper(property_name) like '%&prop_name%'
 order by 1;
