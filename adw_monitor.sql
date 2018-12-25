cfcb4eee23f421285cf3abf368bb74902db2cf0d
KevinOCM99 <dbaami@hotmail.com>
2018-12-25 01:49:03 -0500

    adw enhancement

--- adw_monitor.sql
+++ adw_monitor.sql
@@ -1,3 +1,6 @@
+--elapsed time not work
+--no io
+
 select status, username,to_char(sql_exec_start,'yyyy-mm-dd hh24:mi:ss') start_time, sql_id,rm_consumer_group,px_maxdop,elapsed_time, (elapsed_time + queuing_time + cpu_time)
 from V$SQL_MONITOR 
 where username is not null and username <> 'C##CLOUD$SERVICE' 
