da139f8083a16f586340ca1e6967f4ab0d02d77a
KevinOCM99 <dbaami@hotmail.com>
2018-12-25 21:32:57 -0500

    adw enhancement

--- adw_monitor.sql
+++ adw_monitor.sql
@@ -1,7 +1,10 @@
 --elapsed time not work
 --no io
 
-select status, username,to_char(sql_exec_start,'yyyy-mm-dd hh24:mi:ss') start_time, sql_id,rm_consumer_group,px_maxdop,elapsed_time, (elapsed_time + queuing_time + cpu_time)
+col username for a20;
+col RM_CONSUMER_GROUP for a20 heading CONSUMER_GROUP;
+col 
+select status, username,to_char(sql_exec_start,'yyyy-mm-dd hh24:mi:ss') start_time, sql_id,rm_consumer_group,px_maxdop,elapsed_time, (elapsed_time + queuing_time + cpu_time) all_time_sum
 from V$SQL_MONITOR 
 where username is not null and username <> 'C##CLOUD$SERVICE' 
 order by username,sql_exec_start desc;
