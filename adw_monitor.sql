0057c720f50d2c4cf0e206dadef82a02f9fbf535
KevinOCM99 <dbaami@hotmail.com>
2019-01-07 23:08:29 -0500

    adw enhancement

--- adw_monitor.sql
+++ adw_monitor.sql
@@ -3,7 +3,8 @@
 
 col username for a20;
 col RM_CONSUMER_GROUP for a20 heading CONSUMER_GROUP;
-select status, username,to_char(sql_exec_start,'yyyy-mm-dd hh24:mi:ss') start_time, sql_id,rm_consumer_group,px_maxdop,elapsed_time, (elapsed_time + queuing_time + cpu_time) all_time_sum
+col status for a30;
+select SQL_ID,SQL_EXEC_ID,to_char(sql_exec_start,'yyyy-mm-dd hh24:mi:ss') start_time,status, username, rm_consumer_group,px_maxdop,elapsed_time, (elapsed_time + queuing_time + cpu_time) all_time_sum
 from V$SQL_MONITOR 
 where username is not null and username <> 'C##CLOUD$SERVICE' 
 order by username,sql_exec_start desc;
