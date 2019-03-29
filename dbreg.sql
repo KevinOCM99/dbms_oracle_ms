cb13bb1b514be394d82bc2227895aa4f8eda0882
KevinOCM99 <dbaami@hotmail.com>
2019-03-29 07:39:23 -0400

    adw enhancement

--- dbreg.sql
+++ dbreg.sql
@@ -1,7 +1,8 @@
 col comp_name for a40;
 col status for a20;
 col version for a10;
-select comp_name, status, substr(version,1,10) as version 
+col comp_id for a30
+select comp_id, comp_name, status, substr(version,1,10) as version 
 from dba_registry 
 order by 1;
 
