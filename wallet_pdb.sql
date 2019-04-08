c5ecf699b4b38238273afdd460bd3f5d32aabe69
KevinOCM99 <dbaami@hotmail.com>
2019-04-08 11:13:59 -0400

    pdb re key

---
+++ wallet_pdb.sql
@@ -0,0 +1,7 @@
+!mv &wal_loc/cwallet.sso &wal_loc/cwallet.sso.orig
+administer key management set keystore close;
+administer key management set keystore open identified by "WelcomE__1234567" container=all;
+administer key management set key identified by "WelcomE__1234567" with backup container=all;
+administer key management create auto_login keystore from keystore '&wal_loc' identified by "WelcomE__1234567";
+administer key management set keystore close identified by "WelcomE__1234567" container=all;
+
