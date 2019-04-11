7b14854e4109fe2627b6d0af1a3075a6e72bb902
KevinOCM99 <dbaami@hotmail.com>
2019-04-11 02:49:23 -0400

    zdlra version

--- zdlra_ver.sql
+++ zdlra_ver.sql
@@ -1,9 +1,10 @@
 set echo off;
+set lines 180;
 COLUMN current_time FORMAT A20
 COLUMN version FORMAT A70
 SET NUMF 99999999999
 SELECT value version,
        d.name,
        TO_CHAR(SYSDATE, 'DD-MON-YYYY HH24:MI:SS') current_time
- FROM rasys.config c, v$database d
- WHERE c.name = '_build';
+FROM rasys.config c, v$database d
+WHERE c.name = '_build';
