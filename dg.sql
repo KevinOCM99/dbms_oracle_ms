838b0aded237b5004fd60f5df95d98f97b52cf62
KevinOCM99 <dbaami@hotmail.com>
2019-04-26 00:56:39 -0400

    update dg script to go align with cluster database

--- dg.sql
+++ dg.sql
@@ -1,24 +1,26 @@
 set lines 180
 col inst_id for 999999
 col protection_mode for a20    
-col protection_level for a15
+col protection_level for a20
 col remote_archive for a10
-col database_role for a20
+col database_role for a15
 col dataguard_broker for a15               
 col guard_status for a10 heading GUARD
 col switchover_status for a15 heading CONV_TO
-select protection_mode,protection_level,remote_archive,database_role,
+select INST_ID,protection_mode,protection_level,remote_archive,database_role,
        dataguard_broker,guard_status,switchover_status
-from v$database;
+from gv$database
+order by 1;
 
 
 col first_time for a20;
 col FIRST_CHANGE# for 9999999999999999999;
 
-select FIRST_CHANGE#,FIRST_TIME,SEQUENCE#,DEST_ID,APPLIED
-from v$archived_log
+select THREAD#,FIRST_CHANGE#,FIRST_TIME,SEQUENCE#,DEST_ID,APPLIED
+from gv$archived_log
 where sequence# = (select max(sequence#)
-from v$archived_log
-where applied= 'YES');
+from gv$archived_log
+where applied= 'YES')
+order by THREAD#,FIRST_CHANGE#,DEST_ID;
 
 
--- dgs.sql
+++ dgs.sql
@@ -4,9 +4,9 @@ col DESTINATION for a20;
 col ERROR for a10;
 col PROTECTION_MODE for a20;
 col rmode for a25;
-SELECT replace(DEST_NAME, 'LOG_ARCHIVE_DEST', 'DEST') dest_name,
+SELECT INST_ID,replace(DEST_NAME, 'LOG_ARCHIVE_DEST', 'DEST') dest_name,
        recovery_mode rmode,STATUS,PROTECTION_MODE,DESTINATION,ERROR,SRL
-FROM V$ARCHIVE_DEST_STATUS
+FROM GV$ARCHIVE_DEST_STATUS
 WHERE DESTINATION IS NOT NULL
-order by dest_id;
+order by inst_id,dest_id;
 --ORDER BY to_number(substr(dest_name,instr(dest_name,'_',-1) + 1, length(dest_name) - instr(dest_name,'_', -1)));
