40e7b2aaecc6044af0122c91eb685257b1430625
KevinOCM99 <dbaami@hotmail.com>
2019-04-26 03:34:44 -0400

    adg script

--- dg.sql
+++ dg.sql
@@ -7,7 +7,9 @@ col database_role for a15
 col dataguard_broker for a15               
 col guard_status for a10 heading GUARD
 col switchover_status for a15 heading CONV_TO
-select INST_ID,protection_mode,protection_level,remote_archive,database_role,
+select INST_ID,protection_mode,
+--protection_level,
+remote_archive,database_role,
        dataguard_broker,guard_status,switchover_status
 from gv$database
 order by 1;
