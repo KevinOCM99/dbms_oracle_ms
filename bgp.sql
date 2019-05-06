6e7f469b913d89305abdf26fd0fd203e49b68283
KevinOCM99 <dbaami@hotmail.com>
2019-05-06 13:39:13 -0400

    background process

--- bgp.sql
+++ bgp.sql
@@ -1,8 +1,9 @@
+col ins_id for 99
 col pserial# for 999
 col name for a10
 col description for a50
 col PADDR for a30
-select pserial#,name,description,PADDR
-from v$bgprocess
+select inst_id, pserial#,name,description,PADDR
+from gv$bgprocess
 where paddr <> '00'
-order by 1,2;
+order by 1,3;
