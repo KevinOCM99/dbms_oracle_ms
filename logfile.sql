2c93365c677149110bb665d179e95d9cce04f797
KevinOCM99 <dbaami@hotmail.com>
2019-05-06 14:42:40 -0400

    logfile

--- logfile.sql
+++ logfile.sql
@@ -3,4 +3,5 @@ col inst_id for 99
 col group# for 99
 col status for a15
 col member for a100
-SELECT INST_ID, GROUP#, STATUS, MEMBER FROM GV$LOGFILE order by 1,2;
+col type for a15
+SELECT INST_ID, GROUP#,TYPE, STATUS, MEMBER FROM GV$LOGFILE order by 1,2;
