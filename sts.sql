aac12b5d181f0dff6d91eb83102e3caefa190de8
KevinOCM99 <dbaami@hotmail.com>
2020-01-08 16:33:12 +0800

    Update sts.sql

--- sts.sql
+++ sts.sql
@@ -3,5 +3,9 @@ col owner for a20;
 select name, owner, CREATED, LAST_MODIFIED, statement_count from dba_sqlset order by 2,1;
 
 col sqlset_name for a30;
-col statement_count for 999;
-select count(*) statement_count, sqlset_name from dba_sqlset_statements group by sqlset_name;
+col parsing_schema_name for a20 heading SCHEMA_NAME;
+col sql_text for a80
+select sqlset_name,sql_seq,parsing_schema_name,substr(sql_text,1,80) sql_text 
+from dba_sqlset_statements 
+where parsing_schema_name not in ('LBACSYS','SYS','SYSTEM') 
+order by sqlset_name,sql_seq;
