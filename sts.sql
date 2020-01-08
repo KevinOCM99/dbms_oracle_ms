9ae07feaf63d8fc5887370706cd0406d7c33fb78
KevinOCM99 <dbaami@hotmail.com>
2020-01-08 16:39:52 +0800

    conver clob to char

--- sts.sql
+++ sts.sql
@@ -5,7 +5,7 @@ select name, owner, CREATED, LAST_MODIFIED, statement_count from dba_sqlset orde
 col sqlset_name for a30;
 col parsing_schema_name for a20 heading SCHEMA_NAME;
 col sql_text for a80
-select sqlset_name,sql_seq,parsing_schema_name,substr(sql_text,1,80) sql_text 
+select sqlset_name,sql_seq,parsing_schema_name,to_char(substr(sql_text,1,80)) sql_text 
 from dba_sqlset_statements 
 where parsing_schema_name not in ('LBACSYS','SYS','SYSTEM') 
 order by sqlset_name,sql_seq;
