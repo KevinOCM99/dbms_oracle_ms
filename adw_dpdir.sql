efca0afb0f9f70d0b35a1ce116e2f2ffb8dbe50d
Kevin's MacBook Pro <dbaami@hotmail.com>
2020-10-11 18:33:45 +0800

    adw dp dir

--- adw_dpdir.sql
+++ adw_dpdir.sql
@@ -1,3 +1,4 @@
 col object_name for a50
 col bytes for 999999999999999999
-SELECT * FROM DBMS_CLOUD.LIST_FILES('DATA_PUMP_DIR') order by 1;
+col created for a30
+SELECT object_name,bytes,created FROM DBMS_CLOUD.LIST_FILES('DATA_PUMP_DIR') order by 1;
