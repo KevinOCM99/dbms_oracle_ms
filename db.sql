c0b1201d7121afef660023987362fbbe72cc530f
KevinOCM99999 <dbaami@hotmail.com>
2022-02-11 09:12:09 +0800

    update db.sql

--- db.sql
+++ db.sql
@@ -11,8 +11,9 @@ col instance for a20
 SELECT host_name,
        INSTANCE_NAME INSTANCE,
        sys_context('USERENV','DB_NAME') DB_NAME,
+       status,
        sys_context('USERENV', 'CURRENT_SCHEMA') user_name,
        sys_context('USERENV','HOST') terminal_name
-FROM V$INSTANCE;
+FROM GV$INSTANCE;
 
 @@ver
