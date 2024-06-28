da0b529bc3e3f06ace6e6b8cf7960a8e93069c96
KevinOCM99 <dbaami@hotmail.com>
2024-06-28 11:30:38 +0800

    modify comp

--- db.sql
+++ db.sql
@@ -2,10 +2,11 @@
 -- Created by Junyi Liang 2010/9/17
 set lines 180
 col db_name format a15
-col host_name format a40
+col host_name format a20
 col user_name format a15
 col terminal_name format a30
-col instance for a20
+col instance for a12;
+col status for a10;
 
 
 SELECT host_name,
@@ -13,7 +14,8 @@ SELECT host_name,
        sys_context('USERENV','DB_NAME') DB_NAME,
        status,
        sys_context('USERENV', 'CURRENT_SCHEMA') user_name,
-       sys_context('USERENV','HOST') terminal_name
+       sys_context('USERENV','HOST') terminal_name,
+       logins
 FROM GV$INSTANCE;
 
 @@ver
