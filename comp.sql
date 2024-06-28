e3590eb0b295e87961311f47d2b8f48a36f643b5
KevinOCM99 <dbaami@hotmail.com>
2024-06-28 10:29:46 +0800

    modify comp

--- comp.sql
+++ comp.sql
@@ -1,6 +1,6 @@
-set pages 999 lines 120;
-col comp_name for a50
-col version for a20
-select comp_name,version
-from dba_registry
-order by 1;
+set lines 120 pages 999;
+col comp_id for a10;
+col version for a11;
+col status for a10;
+col comp_name for a37;
+select comp_id,comp_name,version,status from dba_registry;
