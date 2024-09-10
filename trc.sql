9ac3afc46833d550baa8900d4aefa03329b3f40c
KevinOCM99 <dbaami@hotmail.com>
2024-09-10 10:22:33 +0800

    update trc

--- trc.sql
+++ trc.sql
@@ -8,7 +8,8 @@ where name = 'Diag Trace';
 
 SELECT VALUE FROM V$DIAG_INFO WHERE NAME = 'Default Trace File';
 
-select '$ORACLE_BASE/diag/rdbms/your_db/your_sid/trace/alert_yoursid.log' value from dual;
+col value for a120;
+select '$ORACLE_BASE/diag/rdbms/your_db/your_sid/trace/alert_yoursid.log' value from dual
 union
 select '%ORACLE_BASE%\diag\rdbms\%Your_DB%\%Your_Instance%\trace\alert_%Your_Instance%.log' value from dual;
 
