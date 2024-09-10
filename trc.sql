1adcd1369228665e4700ec3f9d7fdce607707c88
KevinOCM99 <dbaami@hotmail.com>
2024-09-10 10:17:11 +0800

    trc

---
+++ trc.sql
@@ -0,0 +1,14 @@
+set lines 120 pages 999
+col INST_ID for 999;
+col NAME for a45;
+col VALUE for a65;
+select INST_ID,NAME,VALUE  
+from V$DIAG_INFO
+where name = 'Diag Trace';
+
+SELECT VALUE FROM V$DIAG_INFO WHERE NAME = 'Default Trace File';
+
+select '$ORACLE_BASE/diag/rdbms/your_db/your_sid/trace/alert_yoursid.log' value from dual;
+union
+select '%ORACLE_BASE%\diag\rdbms\%Your_DB%\%Your_Instance%\trace\alert_%Your_Instance%.log' value from dual;
+
