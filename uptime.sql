708301b5e1b78445e0d538a2140cc5964a779d06
KevinOCM99 <dbaami@hotmail.com>
2024-11-05 10:29:06 +0800

    uptime

--- uptime.sql
+++ uptime.sql
@@ -1,3 +1,4 @@
+set lines 180 pages 999
 col Instance_Name for a13 heading Instance_Name
 col Started_At for a20
 col "Database_Uptime" for a40
@@ -6,7 +7,7 @@ col SESSIONTIMEZONE for a10
 col DBTIMEZONE for a10
 col SESSIONTIMEZONE for a10 heading MYTIMEZONE
 select INST_ID,instance_name,
-to_char(startup_time,'DD-MON-YYYY HH24:MI:SS') as "Started_At",
+to_char(startup_time,'YYYY-MM-DD HH24:MI:SS') as "Started_At",
 floor(sysdate - startup_time) || 'days(s) '|| trunc( 24*((sysdate-startup_time) - trunc(sysdate-startup_time))) || 'hour(s) '|| mod(trunc(1440*((sysdate-startup_time) - trunc(sysdate-startup_time))), 60) ||'minute(s) '|| mod(trunc(86400*((sysdate-startup_time) - trunc(sysdate-startup_time))), 60) ||'seconds'as "Database_Uptime"
 ,timestamp_to_scn(sysdate) current_scn,
 to_char(CURRENT_DATE,'yyyy-mm-dd hh24:mi:ss') Current_date, DBTIMEZONE, SESSIONTIMEZONE
